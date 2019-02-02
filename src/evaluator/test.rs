5f748320c38ee8547df1b6dba966cb49f9ead7ad
Riku Inoue <riku.inoue@shiroyagi.co.jp>
2019-02-02 11:44:05 +0900

    Add test for bool evaluate

--- src/evaluator/test.rs
+++ src/evaluator/test.rs
@@ -3,19 +3,6 @@ use crate::object::Object;
 use crate::lexer::Lexer;
 use crate::parser::Parser;
 
-#[test]
-fn test_eval_int_expr() {
-    let test_cases = vec![
-        ("5", 5),
-        ("10", 10)
-    ];
-
-    for (input, expect) in test_cases {
-        let evaluated = test_eval(input).unwrap();
-        test_int_obj(evaluated, expect);
-    }
-}
-
 fn test_eval(input: &str) -> Option<Object> {
     let mut l = Lexer::new(input.to_string()).unwrap();
     let mut p = Parser::new(&mut l);
@@ -29,5 +16,39 @@ fn test_int_obj(obj: Object, expect: i64) {
         assert_eq!(val, expect);
     } else {
         unreachable!();
+    }
+}
+
+fn test_bool_obj(obj: Object, expect: bool) {
+    if let Object::Bool(val) = obj {
+        assert_eq!(val, expect);
+    } else {
+        unreachable!()
+    }
+}
+
+#[test]
+fn test_eval_int_expr() {
+    let test_cases = vec![
+        ("5", 5),
+        ("10", 10)
+    ];
+
+    for (input, expect) in test_cases {
+        let evaluated = test_eval(input).unwrap();
+        test_int_obj(evaluated, expect);
+    }
+}
+
+#[test]
+fn test_eval_bool_expr() {
+    let test_cases = vec![
+        ("true", true),
+        ("false", false)
+    ];
+
+    for (input, expect) in test_cases {
+        let evaluated = test_eval(input).unwrap();
+        test_bool_obj(evaluated, expect);
     }
 }
